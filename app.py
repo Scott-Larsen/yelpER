8c20cbd93ad39f069e37eee6f5a28f1e628e6919
Scott <scott@scottlarsen.com>
2020-02-04 12:56:47 -0800

    broken but backing up before I make changes to CSS to try to iron out footer.

--- app.py
+++ app.py
@@ -20,34 +20,36 @@ stars = ["i-stars--regular-1__373c0__1HqiV, i-stars--regular-1-half__373c0__1Ght
     "i-stars--regular-4__373c0__2YrSK", "i-stars--regular-4-half__373c0__1YrPo",
     "i-stars--regular-5__373c0__N5JxY"]
 
-def make_reservation(event, lot, number):
-    l = lots_to_rows[lot]
-    worksheet(event).update_cell(l,4, number)
+# def make_reservation(event, lot, number):
+#     l = lots_to_rows[lot]
+#     worksheet(event).update_cell(l,4, number)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index(result=None):
-    if request.args.get('businessType', None) and request.args.get('gps1', None) and request.args.get('gps2', None):
+    # if request.args.get('businessType', None) and request.args.get('gps1', None) and request.args.get('gps2', None):
+    if request.args.get('gps2', None):
+        print(request.args.get('gps2', None))
     # if request.args.get('gps1', None):
         # result = type(request.args['gps1'])
         gps1, gps2 = request.args['gps1'], request.args['gps2']
         gps1 = [float(e) for e in gps1.split(',')]
         gps2 = [float(e) for e in gps2.split(',')]
-        print(gps1, gps2)
-        print(type(gps1), type(gps2))
-        print(type(gps1[0]), type(gps2[0]))
-        print(gps1[0], gps2[0])
-        print(gps1[0] - gps2[0])
-        print((gps1[0] - gps2[0]) ** 2)
-        print(sqrt((gps1[0] - gps2[0]) ** 2 + (gps1[1] - gps2[1]) ** 2))
-        print(sqrt((gps1[0] - gps2[0]) ** 2 + (gps1[1] - gps2[1]) ** 2) * 47)
         
         zoom = sqrt((gps1[0] - gps2[0]) ** 2 + (gps1[1] - gps2[1]) ** 2) * 160
         
-        print(zoom)
+        # print(gps1, gps2)
+        # print(type(gps1), type(gps2))
+        # print(type(gps1[0]), type(gps2[0]))
+        # print(gps1[0], gps2[0])
+        # print(gps1[0] - gps2[0])
+        # print((gps1[0] - gps2[0]) ** 2)
+        # print(sqrt((gps1[0] - gps2[0]) ** 2 + (gps1[1] - gps2[1]) ** 2))
+        # print(sqrt((gps1[0] - gps2[0]) ** 2 + (gps1[1] - gps2[1]) ** 2) * 47)
+        # print(zoom)
 
         return render_template('index.html', restaurants = restaurants, stars = stars, zoom = zoom, gps1 = gps1, gps2 = gps2)
-
+    print("outside the loop")
     return render_template('index.html')
 
 if __name__ == '__main__':
